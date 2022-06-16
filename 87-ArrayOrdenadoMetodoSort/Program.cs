f9677ce5ae17a3b907af7fee80c664cdd6a24e64
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:54:18 -0300

    Add files via upload

---
+++ 87-ArrayOrdenadoMetodoSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+//Dado un array de numeros con el metodo Sort, ordenalos y muestra su contenido.
+
+namespace _87_ArrayOrdenadoMetodoSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3];
+
+            array[0] = 32;
+            array[1] = 27;
+            array[2] = 45;
+
+            Console.WriteLine("Desordenado:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Array.Sort(array);
+
+
+            Console.WriteLine("Ordenado:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
